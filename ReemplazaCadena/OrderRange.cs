08278d7a8b2694924143a1c1645bafa1d3ea5751
DANFER <DANFER@DANFER-PC>
2019-01-21 03:34:11 -0500

    Add project files.

---
+++ ReemplazaCadena/OrderRange.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParesImpares
+{
+    public class OrderRange
+    {
+        public string build(List<int> listaNumeros)
+        {           
+            
+            string cadenaDevolver = "";           
+            var listaPares = new List<int>();
+            var listaImPares = new List<int>();                   
+
+            for (int i = 0; i < listaNumeros.Count(); i++)
+            {
+                if (listaNumeros[i] % 2 == 0)
+                {
+                    listaPares.Add(listaNumeros[i]);                    
+                }
+                else
+                {
+                    listaImPares.Add(listaNumeros[i]);                    
+                }               
+            }
+
+            if (listaPares.Count()>0)
+            {
+                listaPares.Sort();
+                cadenaDevolver = "[";
+                for (int i = 0; i < listaPares.Count(); i++)
+                {
+                    if (i == listaPares.Count() - 1)
+                    {
+                        cadenaDevolver = cadenaDevolver + listaPares[i] + "] ";
+                    }
+                    else
+                    {
+                        cadenaDevolver = cadenaDevolver + listaPares[i] + ",";
+                    }
+                }
+            }
+
+            if (listaImPares.Count() > 0)
+            {
+                listaImPares.Sort();
+                cadenaDevolver = cadenaDevolver + "[";
+                for (int i = 0; i < listaImPares.Count(); i++)
+                {
+                    if (i == listaImPares.Count() - 1)
+                    {
+                        cadenaDevolver = cadenaDevolver + listaImPares[i] + "] ";
+                    }
+                    else
+                    {
+                        cadenaDevolver = cadenaDevolver + listaImPares[i] + ",";
+                    }
+                }
+            }            
+
+            return cadenaDevolver;
+
+        }
+
+        static void Main(string[] args)
+        {
+            
+            OrderRange objetoPrueba = new OrderRange();
+            var ListadoNumeros = new List<int>();            
+            Console.WriteLine("Imprimiendo Pares Impares");
+            Console.WriteLine(objetoPrueba.build(ListadoNumeros));           
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ UnitTestGruposParesImpares/UnitTest1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ParesImpares;
+using System.Collections.Generic;
+
+namespace UnitTestGruposParesImpares
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            OrderRange objetoPrueba = new OrderRange();
+            var ListadoNumeros = new List<int>();
+            ListadoNumeros.Add(2);
+            ListadoNumeros.Add(1);
+            ListadoNumeros.Add(4);
+            ListadoNumeros.Add(5);
+            Console.WriteLine(objetoPrueba.build(ListadoNumeros));
+            
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            OrderRange objetoPrueba = new OrderRange();
+            var ListadoNumeros = new List<int>();
+            ListadoNumeros.Add(4);
+            ListadoNumeros.Add(2);
+            ListadoNumeros.Add(9);
+            ListadoNumeros.Add(3);
+            ListadoNumeros.Add(6);
+           
+            Console.WriteLine(objetoPrueba.build(ListadoNumeros));
+
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            OrderRange objetoPrueba = new OrderRange();
+            var ListadoNumeros = new List<int>();
+            ListadoNumeros.Add(58);
+            ListadoNumeros.Add(60);
+            ListadoNumeros.Add(55);
+            ListadoNumeros.Add(48);
+            ListadoNumeros.Add(57);
+            ListadoNumeros.Add(73);            
+            Console.WriteLine(objetoPrueba.build(ListadoNumeros));
+
+        }
+
+    }
+}
